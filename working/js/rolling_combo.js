2ecf7c9da7b02104290887a6c765d5dbb4da1d14
John Demmitt <john_demmitt@outlook.com>
2017-08-12 20:40:43 -0700

    cycle

--- working/js/rolling_combo.js
+++ working/js/rolling_combo.js
@@ -17,16 +17,15 @@ console.log(letters);
 console.log(wheels);
 console.log(dWidth);
 
-    tl.staggerFrom(resL, 2.5, { x: -offscreen, rotation: -2000, transformOrigin: "50% 50%", ease: "Power0.easeOut"}, -0.3, "rollin")
+    tl.staggerFrom(resL, 2.5, { cycle:{x:[ -offscreen, offscreen], rotation:[ -2000, 2000]},transformOrigin: "50% 50%", ease: "Power0.easeOut"}, -0.3, "rollin")
         .staggerFrom(transL, 2.5, { x: -offscreen, rotation: -2000, transformOrigin: "50% 50%", ease: "Power0.easeOut" }, -0.15, "rollin")
-        .staggerFrom(resW, 2.5, { x: -offscreen, rotation: -2000, transformOrigin: "50% 50%", ease: "Power0.easeOut"}, 0.3, "rollin")
+        .staggerFrom(resW, 2.5, { cycle:{x:[ -offscreen, offscreen], rotation:[ -2000, 2000]}, transformOrigin: "50% 50%", ease: "Power0.easeOut"}, 0.3, "rollin")
         .staggerFrom(transW, 2.5, { x: -offscreen, rotation: -2000, transformOrigin: "50% 50%", ease: "Power0.easeOut"}, 0.15, "rollin")
-        .set(letters, {autoAlpha:1})
-        .staggerFrom(resL, 0.5, {opacity:0, ease: "Power0.easeOut"}, -0.0625, "rollin+=2")
-        .staggerFrom(resW, 0.5, {opacity:1, ease: "Power0.easeOut"}, 0.0625, "rollin+=2")
-        .staggerFrom(transL, 0.5, {opacity:0, ease: "Power0.easeOut"}, -0.07, "rollin+=2")
-        .staggerFrom(transW, 0.5, {opacity:1, ease: "Power0.easeOut"}, 0.07, "rollin+=2");
-
+        .staggerFrom(resL, 0.2, {opacity:0, ease: "Power0.easeOut"}, -0.3, "rollin+=2.3")
+        .staggerFrom(resW, 0.2, {opacity:1, ease: "Power0.easeOut"}, 0.3, "rollin+=2.3")
+        .staggerFrom(transL, 0.2, {opacity:0, ease: "Power0.easeOut"}, -0.1, "rollin+=2.3")
+        .staggerFrom(transW, 0.2, {opacity:1, ease: "Power0.easeOut"}, 0.1, "rollin+=2.3");
+tl.timeScale(0.5);
 
 
     /*  .fromTo([res, trans], 3, { x: -width }, { x: center }, "rollin")
